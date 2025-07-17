c77151047278c8e668854231bab2237774ff16d0
mcnadodls <mcnadodls@yandex.ru>
2025-07-17 17:14:31 +0300

    header and footer

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,13 +1,31 @@
 class Menu extends HTMLElement {
-  constructor() {
-    super();
-}
-
-connectedCallback() {
-    fetch('/components/menu/menu.html')
-        .then(response => response.text())
-        .then(html => this.innerHTML =  html)
+    static observedAttributes = ["prop"];
+    constructor() {
+        super();
     }
     
+    attributeChangedCallback(name, oldValue, newValue) {
+        fetch('/components/menu/menu.html')
+            .then(response => response.text())
+            .then(html => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(html, 'text/html');
+                if (newValue == "header"){
+                    const elementToRemove = doc.getElementsByClassName("menu__info_align_footer");
+                    elementToRemove[0]?.remove();
+                    this.innerHTML =  doc.body.innerHTML;
+                }
+                if (newValue == "footer") { 
+                    const elementToRemove = [doc.getElementsByClassName("menu__info_align_header"), doc.getElementsByName("menuLinkSale")];
+                    elementToRemove[0][0].remove();
+                    elementToRemove[1][0].remove();
+                    this.innerHTML =  doc.body.innerHTML;
+                }
+            })     
+    }
 }
+// Логика создания этого кода)) была такая:
+// Элементы можно переиспользовать. Template не подходит т.к. он занимает DOM когда не используется.
+// Можно было бы вставлять верстку напрямую innerHTML = `<div></div>` но такой формат не удобен в редактировании. Поэтому был выбран вариант импорта
+// Парсить отдельные элементы не очень хорошая идея, но по другому я не нашел как это сдеалать адекватно.
 customElements.define('menu-component', Menu);
